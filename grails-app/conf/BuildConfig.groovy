c20fb66cab1049a7015367d9ceda0581dfc08eed
Oliver Hansen <ollukas@gmail.com>
2015-06-11 02:41:12 +0300

    Uuendused

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -34,8 +34,8 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.16'
-       // runtime 'org.postgresql:postgresql:9.3-1102-jdbc41'
-        runtime 'org.postgresql:postgresql:9.2-1002-jdbc4'
+        runtime 'org.postgresql:postgresql:9.3-1102-jdbc41'
+        //runtime 'org.postgresql:postgresql:9.2-1002-jdbc4'
     }
 
     plugins {
--- grails-app/domain/dokumendid/DocCatalog.groovy
+++ grails-app/domain/dokumendid/DocCatalog.groovy
@@ -8,7 +8,12 @@ class DocCatalog {
     DocCatalogType type;
     Integer level;
 
-    static hasMany = [childs:DocCatalog]
+    //DocCatalog childs;
+    Integer upper_catalog_fk;
+
+    //static hasMany = [childs:DocCatalog]
+    /*
+    static belongsTo = [childs:DocCatalog]*/
 
     static mapping = {
         table "doc_catalog"
@@ -19,10 +24,12 @@ class DocCatalog {
 
         type column: 'doc_catalog_type_fk'
         level column: 'level'
-        childs column: 'upper_catalog_fk'
+        upper_catalog_fk column: 'upper_catalog_fk'
+        //parent column: 'upper_catalog_fk'
         //document column: 'document_fk'
     }
 
     static constraints = {
+        //childs nullable: true
     }
 }
